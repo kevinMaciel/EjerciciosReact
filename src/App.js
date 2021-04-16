c95a3cbaee56371c2a350f4bd1aadb3bb970f93b
Kevin <kevinmaciel.daniel@gmail.com>
2021-04-16 14:03:30 -0300

    Primera version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import Movies from './components/movies';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import MovieForm from './components/movieForm';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <NavBar />
+    <main className="container">
+      <Switch>
+        <Route path="/register" component={RegisterForm}></Route>
+        <Route path="/login" component={LoginForm}></Route>
+        <Route path="/movies/:id" component={MovieForm}></Route>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/movies.jsx
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import MoviesTable from './moviesTable';
+import ListGroup from "./common/listGroup";
+import Pagination from './common/pagination';
+import { getMovies } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
+import { paginate } from '../utils/paginate';
+import { Link} from 'react-router-dom';
+import SearchBox from "./searchBox";
+import _ from 'lodash';
+
+
+class Movies extends Component {
+    state = { 
+        movies: [],
+        genres: [],
+        currentPage: 1 ,
+        searchQuery: "",
+        pageSize: 4,
+        sortColumn: { path: 'title', order: 'asc' }
+     };
+
+     componentDidMount() {
+         const genres = [{_id: "", name: 'All Genres' }, ...getGenres()]
+        this.setState({ movies: getMovies(), genres });
+     };
+
+     handleDelete = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({ movies });
+     };
+
+     handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]};
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies });
+     };
+
+     handlePageChange = page => {
+        this.setState({ currentPage : page});
+     };
+
+     handleGenreSelect = genre => {
+        this.setState({ selectedGenre: genre, currentPage: 1 });
+     };
+
+     handleSearch = query => {
+        this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
+      };
+
+     handleSort = sortColumn => {
+         
+        this.setState({ sortColumn })
+     };
+
+     getPageData = () => {
+
+        const {
+            pageSize, 
+            currentPage, 
+            sortColumn,
+            selectedGenre,
+            searchQuery,
+             movies: allMovies
+            } = this.state;
+
+            let filtered = allMovies;
+            if (searchQuery)
+              filtered = allMovies.filter(m =>
+                m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+              );
+            else if (selectedGenre && selectedGenre._id)
+              filtered = allMovies.filter(m => m.genre._id === selectedGenre._id);
+        
+            const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+        
+            const movies = paginate(sorted, currentPage, pageSize);
+        
+            return { totalCount: filtered.length, data: movies };
+     };
+
+    render() { 
+        const { length: count } = this.state.movies;
+        const {pageSize, currentPage, sortColumn, searchQuery } = this.state;
+
+        if (count === 0) return <p>No hay peliculas en la base de datos.</p>;
+
+        const {totalCount, data: movies} = this.getPageData();
+
+        
+
+        return (
+            <div className="row">
+                <div className="col-3">
+                    <ListGroup 
+                        items={this.state.genres}
+                        selectedItem = {this.state.selectedGenre} 
+                        onItemSelect={this.handleGenreSelect}
+                    />
+                </div>
+                <div className="col">
+                    <Link
+                        to="/movies/new"
+                        className="btn btn-primary"
+                        style={{ marginBotton: 20}}
+                    >
+                        New Movie
+                    </Link>
+                <p> Showing {totalCount} movies in the database.</p>
+                <SearchBox value={searchQuery} onChange={this.handleSearch} />
+                <MoviesTable 
+                    movies={movies} 
+                    sortColumn={sortColumn}
+                    onLike={this.handleLike} 
+                    onDelete={this.handleDelete}
+                    onSort={this.handleSort}
+                />
+            
+                <Pagination 
+                    itemsCount={totalCount} 
+                    pageSize={pageSize}
+                    currentPage ={currentPage} 
+                    onPageChange={ this.handlePageChange }
+                />
+                </div>
+
+            
+            </div>
+        );
+    }
+}
+export default Movies;
